ef99dd0f1c84820e7c768bef2e9573d1a477b9a7
EnderBrah18 <144737670+EnderBrah18@users.noreply.github.com>
2024-06-19 14:41:42 -0300

    3D Animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Ebac.Core.Singleton;
 using TMPro;
 using DG.Tweening;
+using static AnimatorManager;
 
 public class PlayerController : Singleton<PlayerController>
 {
@@ -28,11 +29,16 @@ public class PlayerController : Singleton<PlayerController>
 
     public bool invencible = false;
 
+    [Header("Animation")]
+    public AnimatorManager animatorManager;
+
+
     //privates
     private bool _canRun;
     private Vector3 _pos;
     private float _currentSpeed;
     private Vector3 _startPosition;
+    private float _baseSpeedToAnimation = 5;
 
     private void Start()
     {
@@ -56,7 +62,11 @@ public class PlayerController : Singleton<PlayerController>
     {
         if (collision.transform.tag == tagToCheckEnemy)
         {
-            if(!invencible) EndGame();
+            if (!invencible)
+            {
+                MoveBack();
+                EndGame(AnimatorManager.AnimationType.DEAD);
+            }
         }
     }
 
@@ -68,15 +78,22 @@ public class PlayerController : Singleton<PlayerController>
         }
     }
 
-    private void EndGame()
+    private void MoveBack()
+    {
+        transform.DOMoveZ(-1f, .3f).SetRelative();
+    }
+
+    private void EndGame(AnimatorManager.AnimationType animationType = AnimatorManager.AnimationType.IDLE)
     {
         _canRun = false;
         endScreen.SetActive(true);
+        animatorManager.Play(animationType);
     }
 
     public void StartToRun()
     {
         _canRun = true;
+        animatorManager.Play(AnimatorManager.AnimationType.RUN, _currentSpeed / _baseSpeedToAnimation);
     }
 
     #region POWER UPS
